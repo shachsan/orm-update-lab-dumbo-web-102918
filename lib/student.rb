fe2e2fd59ff2daa50089c015a73f0fe78e1d4f0c
SANJAY K SAI <shachsan@hotmail.com>
2018-11-07 03:44:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,11 +25,7 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade INTEGER
-      )
+      DROP TABLE students 
     SQL
     DB[:conn].execute(sql)
   end
