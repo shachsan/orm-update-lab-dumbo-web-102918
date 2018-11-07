f3e583ed2366678537053aae4454d3324a9bac86
SANJAY K SAI <shachsan@hotmail.com>
2018-11-07 03:47:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute(SELECT)
+    @id = DB[:conn].execute(SELECT last)
   end
 
 end
