a60520fe060c7fc438e2869ef239db25f2e05dc0
SANJAY K SAI <shachsan@hotmail.com>
2018-11-07 04:10:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(row[1])
+    student = self.new(row[])
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
